b655be8d378a9a0f0b04c7ec19b67972a8301132
IZUMIRU <izumiruryousuke@gmail.com>
2020-03-22 18:14:35 +0900

    Updates

---
+++ precache-manifest.8a90d8729b206026c7a99cd85356e868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48dfb09d9ff0d88c32acf514ad54c42",
+    "url": "/patternlab/index.html"
+  },
+  {
+    "revision": "2dda9a0493d12bcf4f64",
+    "url": "/patternlab/static/css/2.e4113a95.chunk.css"
+  },
+  {
+    "revision": "3c2f39c7a816100d81b7",
+    "url": "/patternlab/static/css/main.08df6c83.chunk.css"
+  },
+  {
+    "revision": "2dda9a0493d12bcf4f64",
+    "url": "/patternlab/static/js/2.4bf23afd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/patternlab/static/js/2.4bf23afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2f39c7a816100d81b7",
+    "url": "/patternlab/static/js/main.9b2fe077.chunk.js"
+  },
+  {
+    "revision": "55f37fc300f1c31d602a",
+    "url": "/patternlab/static/js/runtime-main.2a8373c7.js"
+  },
+  {
+    "revision": "e82ed23deb6652478f367c34da81783a",
+    "url": "/patternlab/static/media/logo.e82ed23d.svg"
+  }
+]);
