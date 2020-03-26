c373bef4765acb91a5ad40396fedbb5c7b131c90
IZUMIRU <izumiruryousuke@gmail.com>
2020-03-26 22:36:49 +0900

    fix: change css to scss

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import styles from "./style.module.css";
+import styles from "./style.module.scss";
 
 const Header: FC = () => <div className={styles.header} />;
 
